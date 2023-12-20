677181a6a7bc6137bc2375e68f42f22ff1518f22
apel-pompi <apel.pompi@gmail.com>
2023-12-20 13:21:21 +0600

    transaction display

--- vue-expense-tracker/src/components/TansactionList.vue
+++ vue-expense-tracker/src/components/TansactionList.vue
@@ -2,20 +2,27 @@
     <div class="transaction">
         <h3>History</h3>
         <ul id="list" class="list">
-            <li class="minus">
-                Cash <span>-$400</span>
-                <button class="delete-btn">x</button>
-            </li>
-            <li class="plus">
-                Cash <span>$400</span>
+            <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus': 'plus'">
+                {{transaction.text}} <span>-${{transaction.amount}}</span>
                 <button class="delete-btn">x</button>
             </li>
         </ul>
     </div>
 </template>
 
-<script setup>
-
+<script>
+export default{
+    data() {
+        return {
+            transactions: [
+                {id:1,text:'Flower',amount:-19.99},
+                {id:2,text:'Salary',amount:119.00},
+                {id:3,text:'Book',amount:-10.20},
+                {id:4,text:'Camera',amount:200.00},
+            ],
+        };
+    },
+}
 </script>
 
 
